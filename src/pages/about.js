bc890e16b8fb66e8547f48e141205b605d49ede2
ChrisBDK74 <info@kickmypixel.com>
2022-03-06 20:41:51 +0900

    Progress update

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+
+const AboutPage = () => {
+    return (
+    <Layout pageTitle="About Me">
+      <p>I'm a webdesigner and UX designer with more than 15 years of experience in creating
+          websites and consulting on improving the user experience on websites for companies.</p>
+    </Layout>
+    )
+}
+
+export default AboutPage;
